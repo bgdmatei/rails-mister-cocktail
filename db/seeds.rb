4e8a53a289af00dfc339f905c1c05c63b606741b
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-07 14:25:50 +0900

    created the cocktail controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Deleting all seeds"
+Ingredient.delete_all
+
+puts "Creating new ingedientss.."
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+get_ingredients = open(url).read
+cocktails = JSON.parse(get_ingredients)
+drinks = cocktails['drinks']
+
+drinks.each do |drink|
+  Ingredient.create!(name: drink)
+end
+
+puts "..Finished creating"
