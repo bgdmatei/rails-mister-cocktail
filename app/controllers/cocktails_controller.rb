a9d786c7f2f35d8cc12b389b6463ab3374593dfc
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-08 17:09:26 +0900

    finished but the css is not finished

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,8 @@
 class CocktailsController < ApplicationController
+
+  def home
+  end
+
   def index
     @cocktails = Cocktail.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'cocktails#index'
+  root to: 'cocktails#home'
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create, :destroy]
   end
