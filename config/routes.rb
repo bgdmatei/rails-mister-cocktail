3d2c45c03a7cc5f276721abe0c23f1fcb5c37275
bogdanmatei <matei90bogdan@yahoo.com>
2020-01-17 16:47:58 +0200

    added image url to cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'cocktails#home'
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create, :destroy]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,26 @@
 require 'open-uri'
 require 'json'
 
-puts "Deleting all seeds"
-Ingredient.delete_all
+puts "Clearing database..."
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
 
-puts "Creating new ingedientss.."
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-get_ingredients = open(url).read
-cocktails = JSON.parse(get_ingredients)
-drinks = cocktails['drinks']
+puts "Creating new items.."
+ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktails_url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+get_ingredients = open(ingredients_url).read
+get_cocktails = open(cocktails_url).read
 
-drinks.each do |drink|
-  Ingredient.create!(name: drink['strIngredient1'])
+JSON.parse(get_ingredients)["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
 end
 
-puts "..Finished creating"
+JSON.parse(get_cocktails)["drinks"].each do |cocktail|
+  Cocktail.create!(
+    name: cocktail['strDrink'],
+    image_url: cocktail["strDrinkThumb"]
+    )
+end
+
+puts "Items created : #{Cocktail.count}"
